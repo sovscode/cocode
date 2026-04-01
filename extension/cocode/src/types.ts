26cb406f5551d95d24b6cde7f48a8e139cddef42
Kasper Skov Hansen <56992010+kasperskovhansen@users.noreply.github.com>
2026-04-01 11:23:31 +0200

    Prettier extension

--- extension/cocode/src/types.ts
+++ extension/cocode/src/types.ts
@@ -1,5 +1,10 @@
-export type Session = { id: string ; code: number }
-export type QuestionPostResult = { id: string }
-export type Range = { fromLine: number; toLine: number }
-export type Question = { id: string ; content: string; range: Range; language: string }
-export type Answer = { id: string ; text: string }
+export type Session = { id: string; code: number };
+export type QuestionPostResult = { id: string };
+export type Range = { fromLine: number; toLine: number };
+export type Question = {
+  id: string;
+  content: string;
+  range: Range;
+  language: string;
+};
+export type Answer = { id: string; text: string };
